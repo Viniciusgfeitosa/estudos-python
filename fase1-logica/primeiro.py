51286f1c4a170ccfa98f046a9a5b40adb5e8d412
Vinícius Feitosa <Vinigabrielfeitosa@gmail.com>
2026-04-24 15:19:22 -0300

    Add files via upload

---
+++ fase1-logica/primeiro.py
@@ -0,0 +1,5 @@
+# Primeiro programa em Python
+print('Hello World!')
+
+# Solicitando ao usuário que informe a linguagem de programação que está aprendendo
+input('Qual é a linguagem de programacao que voce esta aprendendo?')
