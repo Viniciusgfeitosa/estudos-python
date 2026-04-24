24a1f78d1d39dcfc05a3826bc1f5c5b8d17a86b2
Vinícius Feitosa <Vinigabrielfeitosa@gmail.com>
2026-04-24 15:20:28 -0300

    Delete fase1-logica directory

--- fase1-logica/primeiro.py
+++
@@ -1,5 +0,0 @@
-# Primeiro programa em Python
-print('Hello World!')
-
-# Solicitando ao usuário que informe a linguagem de programação que está aprendendo
-input('Qual é a linguagem de programacao que voce esta aprendendo?')
